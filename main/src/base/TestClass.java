aa2278489d636db243a4fa9e59426708e728df77
Pawel Gasiorek <pawel.gasiorek@beekeeper.io>
2021-11-09 14:05:00 +0100

    commit

--- main/src/base/TestClass.java
+++ main/src/base/TestClass.java
@@ -4,6 +4,9 @@ public class TestClass {
 
     public static void main(String[] args){
         var x=1;
+        var x1=1;
+        var x2=1;
+        var x3=1;
         System.out.println("something");
     }
 }
